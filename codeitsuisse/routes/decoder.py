012e6b0033ec797240dd2fb760b947d76be429a0
lclaam <62134385+lclaam@users.noreply.github.com>
2021-09-26 01:58:49 +0800

    Update decoder.py

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -11,12 +11,13 @@ logger = logging.getLogger(__name__)
 def decoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    values = data.get("possible_values")
-    num = data.get("num_slots")
-    answer = [values[0] for _ in range(num)]
-    result = {
-        "answer": answer
-    }
-    
+#     values = data.get("possible_values")
+#     num = data.get("num_slots")
+#     answer = [values[0] for _ in range(num)]
+#     result = {
+#         "answer": answer
+#     }
+    inputValue = data.get("input")
+    result = inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
