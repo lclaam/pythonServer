71d5f95f092e8167aeda8e1f0c893e82e7bc4e3e
lclaam <62134385+lclaam@users.noreply.github.com>
2021-09-26 01:21:08 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,6 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.parasite
-import codeitsuisse.routes.fixedrace
 import codeitsuisse.routes.decoder
 
