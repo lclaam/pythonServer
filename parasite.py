16591714d92b2f7123a3fdc56001fb587e9a708c
lclaam <62134385+lclaam@users.noreply.github.com>
2021-09-25 12:47:50 +0800

    Update parasite.py

--- parasite.py
+++ parasite.py
@@ -12,7 +12,7 @@ def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = "This is parasite"
+    result = inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
