49bb34983f377ce8900844a33750783e8f79f5c9
lclaam <62134385+lclaam@users.noreply.github.com>
2021-09-26 03:02:12 +0800

    Update decoder.py

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -13,8 +13,8 @@ def decoder():
     logging.info("data sent for evaluation {}".format(data))
     values = data.get("possible_values")
     num = data.get("num_slots")
-    answer = [values[4] for _ in range(num)]
-#     answer = ['l','t','k','l','l']
+#     answer = [values[4] for _ in range(num)]
+    answer = ['r','x','q','q','q']
     # for _ in data["possible_values"]:
     # answer = [ data["possible_values"][0] for _ in range(data["num_slots"])]
 
