c5880dfde810e82c0d6fb9a087a4b923b088e8d9
lclaam <62134385+lclaam@users.noreply.github.com>
2021-09-26 02:06:12 +0800

    Add files via upload

---
+++ codeitsuisse/routes/decoder.py
@@ -0,0 +1,25 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/decoder', methods=['POST'])
+def decoder():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    values = data.get("possible_values")
+    num = data.get("num_slots")
+    answer = [values[0] for _ in range(num)]
+    # for _ in data["possible_values"]:
+    # answer = [ data["possible_values"][0] for _ in range(data["num_slots"])]
+
+    result = {
+        "answer": answer
+    }
+    
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
